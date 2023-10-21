f11a69f4e40fa6fd0feb6806e3ca230ae7f78be8
Akanksha987 <agrawalakanksha23123@gmail.com>
2023-10-21 22:38:58 +0530

    Added delete commands in the code

--- show_tables.sql
+++ show_tables.sql
@@ -1,14 +1,19 @@
 use Reservations;
 
-Select * from Hotels;
-Select * from Guests;
-Select * from Rooms;
-Select * from RoomTypes;
-Select * from Reservations;
-
 UPDATE Rooms
 SET IsAvailable = FALSE
 WHERE RoomID = 1;
 
 
 Select RoomID, IsAvailable from Rooms;
+
+DELETE FROM RoomTypes WHERE RoomTypeID = 5;
+DELETE FROM Rooms WHERE RoomID = 5;
+DELETE FROM Guests WHERE GuestID = 5;
+DELETE FROM Reservations WHERE ReservationID = 5;
+
+Select * from Hotels;
+Select * from Guests;
+Select * from Rooms;
+Select * from RoomTypes;
+Select * from Reservations;
